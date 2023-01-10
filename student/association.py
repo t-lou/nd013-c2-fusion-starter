635dd3d761afde23c64a2a5c581a15c5fab80bb8
Tongxi Lou <tongxi.lou@gmail.com>
2023-01-10 20:56:16 +0100

    correct for local docker

--- student/association.py
+++ student/association.py
@@ -21,7 +21,7 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-import misc.params as params 
+import misc.params as params
 
 class Association:
     '''Data association class with single nearest neighbor association and gating based on Mahalanobis distance'''
@@ -30,15 +30,15 @@ class Association:
         self.unassigned_tracks = []
         self.unassigned_meas = []
         self.impossible = 1e9
-        
+
     def associate(self, track_list, meas_list, KF):
-             
+
         ############
         # TODO Step 3: association:
         # - replace association_matrix with the actual association matrix based on Mahalanobis distance (see below) for all tracks and all measurements
         # - update list of unassigned measurements and unassigned tracks
         ############
-        
+
         # the following only works for at most one track and one measurement
         N = len(track_list)
         M = len(meas_list)
@@ -54,11 +54,11 @@ class Association:
             for j in range(M):
                 self.association_matrix[i, j] = self.MHD(track_list[i], meas_list[j], KF)
         self.association_matrix[self.gating(self.association_matrix, meas_list[0])] = self.impossible
-        
+
         ############
         # END student code
-        ############ 
-                
+        ############
+
     def get_closest_track_and_meas(self):
         ############
         # TODO Step 3: find closest track and measurement:
@@ -73,73 +73,73 @@ class Association:
             np.argmin(self.association_matrix, axis=None),
             self.association_matrix.shape
         )
-        
+
         # remove from list
-        self.unassigned_tracks.remove(update_track) 
+        self.unassigned_tracks.remove(update_track)
         self.unassigned_meas.remove(update_meas)
         self.association_matrix = np.delete(self.association_matrix, update_track, 0)
         self.association_matrix = np.delete(self.association_matrix, update_meas, 1)
-            
+
         ############
         # END student code
-        ############ 
-        return update_track, update_meas     
+        ############
+        return update_track, update_meas
 
-    def gating(self, MHD, sensor): 
+    def gating(self, MHD, sensor):
         ############
         # TODO Step 3: return True if measurement lies inside gate, otherwise False
         ############
-        
+
         return MHD > chi2.ppf(params.gating_threshold, 3)
-        
+
         ############
         # END student code
-        ############ 
-        
+        ############
+
     def MHD(self, track, meas, KF):
         ############
         # TODO Step 3: calculate and return Mahalanobis distance
         ############
-        
+
         obs = meas.sensor.get_H(track.x)
         diff = meas.z - obs * track.x
         S = meas.R + obs * track.P * obs.T
         return diff.T * np.linalg.inv(S) * diff
-        
+
         ############
         # END student code
-        ############ 
-    
+        ############
+
     def associate_and_update(self, manager, meas_list, KF):
         # associate measurements and tracks
         self.associate(manager.track_list, meas_list, KF)
-    
+
         # update associated tracks with measurements
         while self.association_matrix.shape[0]>0 and self.association_matrix.shape[1]>0:
-            
+
             # search for next association between a track and a measurement
             ind_track, ind_meas = self.get_closest_track_and_meas()
             if np.isnan(ind_track):
                 print('---no more associations---')
                 break
             track = manager.track_list[ind_track]
-            
-            # check visibility, only update tracks in fov    
+
+            # check visibility, only update tracks in fov
             if not meas_list[0].sensor.in_fov(track.x):
                 continue
-            
+
             # Kalman update
             print('update track', track.id, 'with', meas_list[ind_meas].sensor.name, 'measurement', ind_meas)
             KF.update(track, meas_list[ind_meas])
-            
-            # update score and track state 
+
+            # update score and track state
             manager.handle_updated_track(track)
-            
+
             # save updated track
             manager.track_list[ind_track] = track
-            
-        # run track management 
+
+        # run track management
         manager.manage_tracks(self.unassigned_tracks, self.unassigned_meas, meas_list)
-        
-        for track in manager.track_list:            
+
+        for track in manager.track_list:
             print('track', track.id, 'score =', track.score)
--- student/trackmanagement.py
+++ student/trackmanagement.py
@@ -20,22 +20,21 @@ import sys
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-import misc.params as params 
+import misc.params as params
 
 class Track:
     '''Track class with state, covariance, id, score'''
     def __init__(self, meas, id):
         print('creating track no.', id)
         M_rot = meas.sensor.sens_to_veh[0:3, 0:3] # rotation matrix from sensor to vehicle coordinates
-        
+
         ############
         # TODO Step 2: initialization:
-        # - replace fixed track initialization values by initialization of x and P based on 
+        # - replace fixed track initialization values by initialization of x and P based on
         # unassigned measurement transformed from sensor to vehicle coordinates
         # - initialize track state and track score with appropriate values
         ############
-
-        pos = np.array([[meas.z[i]] if i < 3 else [1.] for i in range(4)])
+        pos = np.array([[meas.z[i, 0]] if i < 3 else [1.] for i in range(4)])
         pos = (meas.sensor.sens_to_veh * pos)[:3]
 
         self.x = np.ones([params.dim_state, 1])
@@ -52,11 +51,11 @@ class Track:
         self.P = np.matrix(self.P)
         self.state = 'initialized'
         self.score = 1./params.window
-        
+
         ############
         # END student code
-        ############ 
-               
+        ############
+
         # other track attributes
         self.id = id
         self.width = meas.width
@@ -67,13 +66,13 @@ class Track:
 
     def set_x(self, x):
         self.x = x
-        
+
     def set_P(self, P):
-        self.P = P  
-        
+        self.P = P
+
     def set_t(self, t):
-        self.t = t  
-        
+        self.t = t
+
     def update_attributes(self, meas):
         # use exponential sliding average to estimate dimensions and orientation
         if meas.sensor.name == 'lidar':
@@ -83,9 +82,9 @@ class Track:
             self.height = c*meas.height + (1 - c)*self.height
             M_rot = meas.sensor.sens_to_veh
             self.yaw = np.arccos(M_rot[0,0]*np.cos(meas.yaw) + M_rot[0,1]*np.sin(meas.yaw)) # transform rotation from sensor to vehicle coordinates
-        
-        
-###################        
+
+
+###################
 
 class Trackmanagement:
     '''Track manager with logic for initializing and deleting objects'''
@@ -94,19 +93,19 @@ class Trackmanagement:
         self.track_list = []
         self.last_id = -1
         self.result_list = []
-        
-    def manage_tracks(self, unassigned_tracks, unassigned_meas, meas_list):  
+
+    def manage_tracks(self, unassigned_tracks, unassigned_meas, meas_list):
         ############
         # TODO Step 2: implement track management:
         # - decrease the track score for unassigned tracks
         # - delete tracks if the score is too low or P is too big (check params.py for parameters that might be helpful, but
         # feel free to define your own parameters)
         ############
-        
+
         # decrease score for unassigned tracks
         for i in unassigned_tracks:
             track = self.track_list[i]
-            # check visibility    
+            # check visibility
             if meas_list: # if not empty
                 if meas_list[0].sensor.in_fov(track.x):
                     # your code goes here
@@ -120,13 +119,13 @@ class Trackmanagement:
 
         ############
         # END student code
-        ############ 
-            
+        ############
+
         # initialize new track with unassigned measurement
-        for j in unassigned_meas: 
+        for j in unassigned_meas:
             if meas_list[j].sensor.name == 'lidar': # only initialize with lidar measurements
                 self.init_track(meas_list[j])
-            
+
     def addTrackToList(self, track):
         self.track_list.append(track)
         self.N += 1
@@ -139,8 +138,8 @@ class Trackmanagement:
     def delete_track(self, track):
         print('deleting track no.', track.id)
         self.track_list.remove(track)
-        
-    def handle_updated_track(self, track):      
+
+    def handle_updated_track(self, track):
         ############
         # TODO Step 2: implement track management for updated tracks:
         # - increase track score
@@ -150,7 +149,7 @@ class Trackmanagement:
         track.score += 1./params.window
 
         track.state = 'confirmed' if track.score > params.confirmed_threshold else 'tentative'
-        
+
         ############
         # END student code
-        ############ 
+        ############
