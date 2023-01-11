c28ba02fb1298cd04d1a9b2541b3bd562748ff44
Tongxi Lou <tongxi.lou@gmail.com>
2023-01-11 19:29:26 +0100

    correct cam proj

--- student/measurements.py
+++ student/measurements.py
@@ -87,7 +87,7 @@ class Sensor:
             pos_sens /= pos_sens[0]
             return np.array([
                 [self.c_i - float(pos_sens[1]) * self.f_i],
-                [self.c_i - float(pos_sens[2]) * self.f_i]])
+                [self.c_j - float(pos_sens[2]) * self.f_j]])
 
             ############
             # END student code
